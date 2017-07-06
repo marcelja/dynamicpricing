aa97ff981af99080addc86f18a0043da03ea55a7
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2017-07-07 01:43:05 +0200

    mlp merchant

--- merchant/mlp.py
+++ merchant/mlp.py
@@ -3,8 +3,7 @@ from merchant_sdk import MerchantBaseLogic, MerchantServer
 import os
 import logging
 import argparse
-from sklearn.ensemble import RandomForestRegressor
-
+from sklearn.neural_network import MLPRegressor
 
 if os.getenv('API_TOKEN'):
     merchant_token = os.getenv('API_TOKEN')
@@ -23,11 +22,11 @@ settings = {
     'primeShipping': 1,
     'max_req_per_sec': 10.0,
     'learning_interval': 2.0,
-    'data_file': 'rand_for_models.pkl'
+    'data_file': 'mlp_models.pkl'
 }
 
 
-class RandomForestMerchant(MLMerchant):
+class MLPMerchant(MLMerchant):
     def __init__(self):
         self.model = dict()
         super().__init__(merchant_token, settings)
@@ -36,26 +35,28 @@ class RandomForestMerchant(MLMerchant):
         # TODO include time and amount of sold items to featurelist
         logging.debug('Start training')
         for product_id, vector_tuple in features.items():
-            # I'm puttin n_estimators in constructor,
-            # since this is a good point for improvement (btw. 10 is actually default)
-            product_model = RandomForestRegressor(n_estimators=10)
+            # More configuration needed here
+            product_model = MLPRegressor(hidden_layer_sizes=(10,), max_iter=100000)
+            # TODO: what does partial_fit() do?
             product_model.fit(vector_tuple[0], vector_tuple[1])
             self.model[product_id] = product_model
         logging.debug('Finished training')
 
     def predict(self, product_id, situations):
         # TODO: What happens if there is no such product_id ?
+        # TODO2: It's possible, that predict return negative possibility,
+        #        that's actually not possible
         return self.model[product_id].predict(situations)
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
     parser = argparse.ArgumentParser(
-        description='PriceWars Merchant doing Random Forest Regression')
+        description='PriceWars Merchant doing MLP Regression')
     parser.add_argument('--port',
                         type=int,
-                        default=5102,
+                        default=5103,
                         help='Port to bind flask App to, default is 5101')
     args = parser.parse_args()
-    server = MerchantServer(RandomForestMerchant())
+    server = MerchantServer(MLPMerchant())
     app = server.app
     app.run(host='0.0.0.0', port=args.port)
